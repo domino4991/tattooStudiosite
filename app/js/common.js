03efc7c6530718b8e8ac7e085be63301b7b6ec9f
Kochkin Vladislav <ltrickfaim@gmail.com>
2018-08-09 17:56:03 +0600

    add artists block

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,11 @@ $(function() {
       items: 1,
       dots: true
   })
+  $('.all-artists').owlCarousel({
+    items: 6,
+    nav: true,
+    dots: false,
+    loop: true
+  })
+  $('.all-artists').find('.owl-item').addClass('all-artists-item');
 })
