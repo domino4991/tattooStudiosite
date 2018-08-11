8c064d55330842efa3a08267649e179fc26b5381
Kochkin Vladislav <ltrickfaim@gmail.com>
2018-08-12 00:44:47 +0600

    rebuild js and style some blocks

--- app/js/common.js
+++ app/js/common.js
@@ -25,4 +25,33 @@ $(function() {
      $('.top-line__nav').toggleClass('if-active');
      $('.nav__item').toggleClass('if-active-item');
    })
+
+   $('.flowing-scroll').on( 'click', function(){ 
+    let el = $(this);
+    let dest = el.attr('href'); // получаем направление
+    if(dest !== undefined && dest !== '') { // проверяем существование
+        $('html').animate({ 
+            scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+        }, 1000 // скорость прокрутки
+        );
+    }
+    return false;
+    });
+
+    let target = $('.artists');
+    let targetPos = target.offset().top;
+    let winHeight = $(window).height();
+    let scrollToElem = targetPos - winHeight;
+    $(window).scroll(function(){
+      var winScrollTop = $(this).scrollTop();
+      if(winScrollTop > scrollToElem){
+        $('.scroll-top').css({
+          display: 'block'
+        });
+      } else {
+        $('.scroll-top').css({
+              display: 'none'
+        });
+      }
+    });
 });
